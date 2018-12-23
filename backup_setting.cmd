89ad73ddae7603766b1ed9af40934f2ece6ddb79
Va25dim <46086686+Va25dim@users.noreply.github.com>
2018-12-23 17:00:41 +0300

    Update backup_setting.cmd

--- backup_setting.cmd
+++ backup_setting.cmd
@@ -1,6 +1,8 @@
-rem Set DateTime=%date:~8,2%.%date:~3,2%.%date:~0,2%-%time:~0,2%.%time:~3,2%
+Set AllDateTime=%date:~6,4%.%date:~3,2%.%date:~0,2%-%time:~0,2%.%time:~3,2%
 Set DateTime=%date:~6,4%.%date:~3,2%.%date:~0,2%
 set HOURtime=%time:~0,2%
+Set DayTime=%date:~0,2%
+Set HMTime=%time:~0,2%-%time:~3,2%
 set BackUp_DIR=backup
 set 7z_parm=a -t7z -m=LZMA -ssw -mx9 -pPassword -r0
 ser work_dir="c:\Users\Вадим\AppData\Roaming\uTorrent\"
@@ -22,9 +24,21 @@ cd %current_dir%
 
 cd %work_dir%
 IF NOT EXIST "%BackUp_DIR%\_HOUR\%hourTime%" (
-rmdir /Q /S %BackUp_DIR%\_HOUR\)
+rmdir /Q /S %BackUp_DIR%\_HOUR\
+IF NOT EXIST "%BackUp_DIR%\_DAY\%DayTime%" (
+rmdir /Q /S %BackUp_DIR%\_DAY\ 
+)
+mkdir %BackUp_DIR%\_DAY\%DayTime%
+mkdir %BackUp_DIR%\_DAY\%AllDateTime%
+xcopy  "*.dat" "%BackUp_DIR%\_DAY\%AllDateTime%"
+xcopy  "*.old" "%BackUp_DIR%\_DAY\%AllDateTime%"
+xcopy  "*.bad" "%BackUp_DIR%\_DAY\%AllDateTime%"
+
+)
 mkdir %BackUp_DIR%\_HOUR\%hourTime%
-xcopy  "*.dat" "%BackUp_DIR%\_HOUR\%hourTime%
-xcopy  "*.old" "%BackUp_DIR%\_HOUR\%hourTime%
-xcopy  "*.bad" "%BackUp_DIR%\_HOUR\%hourTime%
+mkdir %BackUp_DIR%\_HOUR\%AllDateTime%
+xcopy  "*.dat" "%BackUp_DIR%\_HOUR\%AllDateTime%"
+xcopy  "*.old" "%BackUp_DIR%\_HOUR\%AllDateTime%"
+xcopy  "*.bad" "%BackUp_DIR%\_HOUR\%AllDateTime%"
+
 
